b8b8e2c55f498cb47e819772d9b400f4f2d37dca
leila <mbiydzenyuyeileen@gmail.com>
2025-01-20 20:50:17 +0100

    added the linter .github folder

---
+++ src/components/header.jsx
@@ -0,0 +1,18 @@
+function Header() {
+  return (
+    <header>
+      <div className="header">
+        <span className="logo">COSMETICS <span>.M</span></span>
+        <nav>
+          <li>Purchase</li>
+          <li>About</li>
+          <li>Contact</li>
+        </nav>
+        <button>Buy Now</button>
+      </div>
+    </header>
+   
+  )
+}
+
+export default Header
